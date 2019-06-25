629efe249e4f7edcfa163311af0b9a677edd3efa
abhiram <abhirammadugula93@gmail.com>
2019-06-24 22:40:10 -0500

    Obtaining the cell details for the edit nutrients table view controller.

--- RationBalancing/RationBalancing/SliderTableViewCell.swift
+++ RationBalancing/RationBalancing/SliderTableViewCell.swift
@@ -10,20 +10,20 @@ import UIKit
 
 class SliderTableViewCell: UITableViewCell {
 
-//    @IBOutlet weak var nutrientLabel: UILabel!
-//    @IBOutlet weak var cellSlider1: UISlider!
-//    @IBOutlet weak var cellSlider2: UISlider!
-//
-//
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-//
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
+    @IBOutlet weak var nutrientLabel: UILabel!
+    @IBOutlet weak var cellSlider1: UISlider!
+    @IBOutlet weak var cellSlider2: UISlider!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
 
 }
