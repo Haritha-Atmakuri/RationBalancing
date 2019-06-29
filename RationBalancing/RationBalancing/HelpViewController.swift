1cd15784425e5357e9bbf50f5789573446890884
abhiram <abhirammadugula93@gmail.com>
2019-06-29 01:20:27 -0500

    Add Help View Controller

--- RationBalancing/RationBalancing/HelpViewController.swift
+++ RationBalancing/RationBalancing/HelpViewController.swift
@@ -12,46 +12,32 @@ class HelpViewController: UIViewController {
 
     @IBOutlet weak var HelpTextView: UITextView!
     override func viewDidLoad() {
-//
-//
-//
-//        let shownString = NSMutableAttributedString(string: "Please  click  here to know more on calculations of Ration Balancing")
-//        let url = URL(string: "http://www.ansc.purdue.edu/compute/balance.htm")!
-//
-//        // Set the 'click here' substring to be the link
-//        shownString.setAttributes([.link: url], range: NSMakeRange(7, 12))
-//
-//        HelpTextView.attributedText = shownString
-//        HelpTextView.isUserInteractionEnabled = true
-//        HelpTextView.isEditable = false
-//
-//        // Set how links should appear: blue and underlined
-//        HelpTextView.linkTextAttributes = [
-//            .foregroundColor: UIColor.blue,
-//            .underlineStyle: NSUnderlineStyle.single.rawValue
-//        ]
-//        super.viewDidLoad()
-//        let background = UIImage(named: "cattle1")
-//        var imageView : UIImageView!
-//        imageView = UIImageView(frame: view.bounds)
-//        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
-//        imageView.clipsToBounds = true
-//        imageView.image = background
-//        imageView.center = view.center
-//        view.addSubview(imageView)
-//        self.view.sendSubviewToBack(imageView)
+        let shownString = NSMutableAttributedString(string: "Please  click  here to know more on calculations of Ration Balancing")
+        let url = URL(string: "http://www.ansc.purdue.edu/compute/balance.htm")!
+
+        // Set the 'click here' substring to be the link
+        shownString.setAttributes([.link: url], range: NSMakeRange(7, 12))
+
+        HelpTextView.attributedText = shownString
+        HelpTextView.isUserInteractionEnabled = true
+        HelpTextView.isEditable = false
+
+        // Set how links should appear: blue and underlined
+        HelpTextView.linkTextAttributes = [
+            .foregroundColor: UIColor.blue,
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        super.viewDidLoad()
+        let background = UIImage(named: "cattle1")
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
