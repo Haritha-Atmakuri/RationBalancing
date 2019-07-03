11d48c4bbdd6610c747e02d760c6bdf356576fee
Suresh <S533575@nwmissouri.edu>
2019-07-03 03:19:07 -0500

    Implemented logic in finding the coordinate location of + and - button in edit nutrients page

--- RationBalancing/RationBalancing/EditNutrientsTableViewController.swift
+++ RationBalancing/RationBalancing/EditNutrientsTableViewController.swift
@@ -152,34 +152,27 @@ class EditNutrientsTableViewController: UITableViewController {
 //            self.nutrients1.append(Nutreints(cattleWeight: value1!, growthRequirement: value2!, nutrientType: cell.name.text!, minvalue: Float(cell.value.text!)!))
         }
         print(nutrients1)
-        self.tabBarController?.selectedIndex = 0
+        self.tabBarController?.selectedIndex = 1
     }
 }
 
 
-//public extension UITableView {
-//
-//    /**
-//     This method returns the indexPath of the cell that contains the specified view
-//     - Parameter view: The view to find.
-//     - Returns: The indexPath of the cell containing the view, or nil if it can't be found
-//     */
-//
-//    func indexPathForView(_ view: UIView) -> IndexPath? {
-//        let center = view.center
-//        print("center: \(center)")
-//        //The center of the view is a better point to use, but we can only use it if the view has a superview
-//        guard let superview = view.superview else {
-//            //The view we were passed does not have a valid superview.
-//            //Use the view's bounds.origin and convert from the view's coordinate system
-//            let origin = self.convert(view.bounds.origin, from: view)
-//            let indexPath = self.indexPathForRow(at: origin)
-//            return indexPath
-//        }
-//        let viewCenter = self.convert(center, from: superview)
-//        //        print("viewcenter: \(viewCenter)")
-//        let indexPath = self.indexPathForRow(at: viewCenter)
-//        //        print("After loop: \(indexPath)")
-//        return indexPath
-//    }
-//}
+public extension UITableView {
+    func indexPathForView(_ view: UIView) -> IndexPath? {
+        let center = view.center
+        print("center: \(center)")
+        //The center of the view is a better point to use, but we can only use it if the view has a superview
+        guard let superview = view.superview else {
+            //The view we were passed does not have a valid superview.
+            //Use the view's bounds.origin and convert from the view's coordinate system
+            let origin = self.convert(view.bounds.origin, from: view)
+            let indexPath = self.indexPathForRow(at: origin)
+            return indexPath
+        }
+        let viewCenter = self.convert(center, from: superview)
+        //        print("viewcenter: \(viewCenter)")
+        let indexPath = self.indexPathForRow(at: viewCenter)
+        //        print("After loop: \(indexPath)")
+        return indexPath
+    }
+}
---
+++ RationBalancing/RationBalancing/ResultViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultViewController.swift
+//  RationBalancing
+//
+//  Created by student on 7/3/19.
+//  Copyright © 2019 NWMSU. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var result1TVC: UITableView!
+    @IBOutlet weak var result2TVC: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
